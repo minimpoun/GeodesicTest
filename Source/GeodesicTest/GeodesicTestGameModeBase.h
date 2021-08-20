693180dd98bc6c70cee1953bf461a46930991169
Chris <chris@zephyr.gg>
2021-08-19 20:19:01 -0500

    init commit

---
+++ Source/GeodesicTest/GeodesicTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GeodesicTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GEODESICTEST_API AGeodesicTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
